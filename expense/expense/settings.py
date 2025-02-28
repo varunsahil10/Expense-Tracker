e9dd2400b3ad16298068c72e36c6165ccaeac188
varun <varunsahil10@gmail.com>
2025-02-28 18:02:49 +0530

    Setup MySQL DB and create model

--- expense/expense/settings.py
+++ expense/expense/settings.py
@@ -75,11 +75,23 @@ WSGI_APPLICATION = 'expense.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'mydb',
+		'USER': 'root',
+		'PASSWORD': 'admin',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
