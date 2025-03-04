84b0fd10071ba3c832c105c599d1ea0778621ce6
varun <varunsahil10@gmail.com>
2025-03-04 14:13:53 +0530

    Setup URL

--- expense/tracker/models.py
+++ expense/tracker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 
 class BaseModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -12,3 +12,6 @@ class BaseModel(models.Model):
 class Transaction(BaseModel):
     description = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10,decimal_places=2)
+
+    class Meta:
+        ordering = ['description']
