bffe4d4f54183e66130e401accdcb89f46f679e5
varun <varunsahil10@gmail.com>
2025-03-04 14:54:55 +0530

    POST method

--- expense/tracker/views.py
+++ expense/tracker/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 # Create your views here.
 def index(request):
+
+    if request.method == "POST":
+        print(request.POST)
+        form = request.POST
+        description = form.get('description')
+        amount = form.get('amount')
+        
+        if not description or amount:
+            return redirect('/')
+
+        print(description,amount)
+    
     return render(request, 'index.html')
